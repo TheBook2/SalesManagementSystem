295a930e5a65aa633daa7e77437742f1a09da6f4
TheBook2 <nkgrimm1@gmail.com>
2026-06-20 19:53:39 +0700

    Extend RegularCustomer

--- SalesManagementSystem/src/model/customer/RegularCustomer.java
+++ SalesManagementSystem/src/model/customer/RegularCustomer.java
@@ -1,5 +1,18 @@
 package model.customer;
 
-public class RegularCustomer {
+public class RegularCustomer extends Customer {
 
+    public RegularCustomer() {
+        setCustomerType("Regular");
+    }
+
+    public RegularCustomer(int id, String name, String phone, String address) {
+        super(id, name, phone, address);
+        setCustomerType("Regular");
+    }
+
+    @Override
+    public double getDiscountRate() {
+        return 0.0;
+    }
 }

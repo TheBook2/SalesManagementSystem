273b3ef17aa9452f3b691972d6f80aad82f2b6e0
TheBook2 <nkgrimm1@gmail.com>
2026-06-20 20:26:36 +0700

    Thay phan transactionItem
    Refactor TransactionItem class to include quantity and update constructor.

--- SalesManagementSystem/src/model/TransactionItem.java
+++ SalesManagementSystem/src/model/TransactionItem.java
@@ -1,9 +1,31 @@
 package model;
 
 public class TransactionItem {
-    private Product itemProduct;
+    private Product product;
+    private int quantity;
 
-    public TransactionItem() {
+    public TransactionItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
     }
-    
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSubTotal() {
+        return product.getPrice() * quantity;
+    }
 }

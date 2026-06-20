2d2ebfe1ae50674edacfd329db035621b09f8f2a
TheBook2 <nkgrimm1@gmail.com>
2026-06-20 20:27:39 +0700

    Thay phan transaction
    Refactor Transaction class to improve readability and add calculateTotal method.

--- SalesManagementSystem/src/model/Transaction.java
+++ SalesManagementSystem/src/model/Transaction.java
@@ -1,43 +1,54 @@
 package model;
 
-import java.time.LocalDate;
-
 public class Transaction {
-    TransactionItem[] lineItems;
-    LocalDate currenDate = LocalDate.now();
+    private TransactionItem[] lineItems;
     private String date;
     private int customerId;
 
     public Transaction() {
     }
 
-    public Transaction(TransactionItem[] lineItems, String date, int customerId) {
+    public Transaction(TransactionItem[] lineItems,
+                       String date,
+                       int customerId) {
         this.lineItems = lineItems;
         this.date = date;
         this.customerId = customerId;
     }
 
-    // Line Items
     public TransactionItem[] getLineItems() {
         return lineItems;
     }
+
     public void setLineItems(TransactionItem[] lineItems) {
         this.lineItems = lineItems;
     }
 
-    // Date
     public String getDate() {
         return date;
     }
+
     public void setDate(String date) {
         this.date = date;
     }
 
-    // Customer ID
     public int getCustomerId() {
         return customerId;
     }
+
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+
+        for (TransactionItem item : lineItems) {
+            if (item != null) {
+                total += item.getSubTotal();
+            }
+        }
+
+        return total;
     }
 }

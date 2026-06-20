d0e5a2e598555993dc9c2a53ff9710eb17d6e968
TheBook2 <nkgrimm1@gmail.com>
2026-06-20 19:55:04 +0700

    Extend VIPCustomer

--- SalesManagementSystem/src/model/customer/VIPCustomer.java
+++ SalesManagementSystem/src/model/customer/VIPCustomer.java
@@ -1,5 +1,20 @@
 package model.customer;
 
-public class VIPCustomer {
+package model.customer;
 
+public class VIPCustomer extends Customer {
+
+    public VIPCustomer() {
+        setCustomerType("VIP");
+    }
+
+    public VIPCustomer(int id, String name, String phone, String address) {
+        super(id, name, phone, address);
+        setCustomerType("VIP");
+    }
+
+    @Override
+    public double getDiscountRate() {
+        return 0.15; // 15%
+    }
 }
